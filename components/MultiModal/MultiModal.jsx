6a27eda5b31639870f81633fa8c5cb6346230946
Руслан <rd@ilab.kz>
2025-07-08 19:35:21 +0500

    -верстка

--- components/MultiModal/MultiModal.jsx
+++ components/MultiModal/MultiModal.jsx
@@ -5,11 +5,49 @@ import {useDispatch, useSelector} from "react-redux";
 import ProjectModal from "@/modals/ProjectModal/ProjectModal";
 import closeModalSvg from '@/public/img/closeModal.svg';
 import Image from "next/image";
+import {useEffect} from "react";
 
 const MultiModal = () => {
   const {modalIsOpen, modalType, modalData} = useSelector(state => state.multiModal.modal);
 
   const dispatch = useDispatch();
+
+
+  useEffect(() => {
+    if (modalIsOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+
+    // Optional cleanup on unmount
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, [modalIsOpen]);
+
+  // Закрытие модального окна по нажатию на Escape
+  const handleKeyDown = (event) => {
+
+    if (event.key === 'Escape') {
+      // Проходимся по массиву c конца , и если модальное окно открыто , то закрываем его и выходим с цикла
+      // Метод find() находит первый открытый модал.
+      const openModal = modalIsOpen === true;
+
+      if (openModal) {
+        dispatch({ type: "CLOSE_MODAL"});
+      }
+
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [modalIsOpen]);
 
   return(
     <div className={`${styles.modal} ${modalIsOpen ? styles.open : ''}`}>
